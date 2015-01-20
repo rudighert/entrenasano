3f3df21471b78a8dc336a977fb754b510e715bf2
Rudighert Schalchli <rudighert@gmail.com>
2015-01-20 19:41:13 -0300

    birthday

--- app/models/pupil.rb
+++ app/models/pupil.rb
@@ -2,9 +2,10 @@ class Pupil
   include MongoMapper::Document
 
   key :email,     required: true
-  key :rut,       required: true
+  key :rut,       required: true, unique: true
   key :name,      required: true
   key :last_name, required: true
+  key :birthday,  Time
 
   belongs_to :trainer
 
