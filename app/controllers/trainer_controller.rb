c00293c822135ddc2cfbdfa6b29c85f4d526a893
Rudighert Schalchli <rudighert@gmail.com>
2015-01-27 18:09:18 -0300

    correct fotmat day

--- app/controllers/trainer_controller.rb
+++ app/controllers/trainer_controller.rb
@@ -11,7 +11,7 @@ class TrainerController < ApplicationController
   def add_pupil
     date = params[:pupil][:birthday].split('/')
     ap date
-    params[:pupil][:birthday] = DateTime.new(date[2].to_i,date[1].to_i,date[0].to_i).utc
+    params[:pupil][:birthday] = DateTime.new(date[2].to_i,date[0].to_i,date[1].to_i).utc
     ap params
     current_trainer.add_pupil params[:pupil]
     redirect_to trainer_index_path
